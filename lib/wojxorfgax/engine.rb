623ef67db9f243169a76882832793b3fc745169d
William Johnston <wjohnston@mpr.org>
2017-04-20 12:19:24 -0500

    Manually include rspec and factory_girl generators

--- lib/wojxorfgax/engine.rb
+++ lib/wojxorfgax/engine.rb
@@ -4,5 +4,10 @@ module Wojxorfgax
   class Engine < ::Rails::Engine
     isolate_namespace Wojxorfgax
     config.generators.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end

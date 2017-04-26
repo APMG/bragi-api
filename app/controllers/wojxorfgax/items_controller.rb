afd3ee466d3620b8fa7565d966a0db8cf25a77ba
William Johnston <wjohnston@mpr.org>
2017-04-26 10:49:08 -0500

    Extract complex controller actions into classes

--- app/controllers/wojxorfgax/items_controller.rb
+++ app/controllers/wojxorfgax/items_controller.rb
@@ -15,31 +15,11 @@ module Wojxorfgax
     end
 
     def update
-      # See if it already exists.
-      item = Item.find_by!(id: params[:id], user: current_user)
-      update_params = permitted_params[:attributes]
-      update_params.delete :audio_identifier
-      item.attributes = update_params
-      if item.played?
-        item.finished = Time.zone.now if item.finished.nil?
-        item.position = nil
-      end
-
-      return if item.save
-      render json: item, status: :bad_request, serializer: ActiveModel::Serializer::ErrorSerializer
+      UpdateAction.new(self).render
     end
 
     def create
-      # See if it already exists.
-      item = Item.find_by(audio_identifier: permitted_params[:attributes][:audio_identifier], user: current_user) || Item.new
-      item.attributes = permitted_params[:attributes]
-      item.user = current_user
-
-      if item.save
-        render json: item
-      else
-        render json: item, status: :bad_request, serializer: ActiveModel::Serializer::ErrorSerializer
-      end
+      CreateAction.new(self).render
     end
 
     def destroy
@@ -47,12 +27,60 @@ module Wojxorfgax
       item.destroy
     end
 
-    private
+    class UpdateAction
+      def initialize(context)
+        @context = context
+      end
 
-    def permitted_params
-      params.require(:data).permit(
-        :type, attributes: %i[after audio_identifier audio_url audio_title audio_description audio_hosts audio_program origin_url source playtime status finished]
-      )
+      def render
+        # See if it already exists.
+        item = Item.find_by!(id: @context.params[:id], user: @context.current_user)
+        update_params = permitted_params[:attributes]
+        update_params.delete :audio_identifier
+        item.attributes = update_params
+        if item.played?
+          item.finished = Time.zone.now if item.finished.nil?
+          item.position = nil
+        end
+
+        return if item.save
+        @context.render json: item, status: :bad_request, serializer: ActiveModel::Serializer::ErrorSerializer
+      end
+
+      private
+
+      def permitted_params
+        @context.params.require(:data).permit(
+          :type, attributes: %i[after audio_identifier audio_url audio_title audio_description audio_hosts audio_program origin_url source playtime status finished]
+        )
+      end
+    end
+
+    class CreateAction
+      def initialize(context)
+        @context = context
+      end
+
+      def render
+        # See if it already exists.
+        item = Item.find_by(audio_identifier: permitted_params[:attributes][:audio_identifier], user: @context.current_user) || Item.new
+        item.attributes = permitted_params[:attributes]
+        item.user = @context.current_user
+
+        if item.save
+          @context.render json: item
+        else
+          @context.render json: item, status: :bad_request, serializer: ActiveModel::Serializer::ErrorSerializer
+        end
+      end
+
+      private
+
+      def permitted_params
+        @context.params.require(:data).permit(
+          :type, attributes: %i[after audio_identifier audio_url audio_title audio_description audio_hosts audio_program origin_url source playtime status finished]
+        )
+      end
     end
   end
 end

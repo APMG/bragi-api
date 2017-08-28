25138e4b797fdcafcd022c35898f9758521ad3c5
William Johnston <wjohnston@mpr.org>
2017-08-28 13:51:33 -0500

    Show after_id in JSON

--- app/serializers/bragi/item_serializer.rb
+++ app/serializers/bragi/item_serializer.rb
@@ -2,10 +2,6 @@
 
 module Bragi
   class ItemSerializer < ActiveModel::Serializer
-    attributes :after, :audio_url, :audio_identifier, :audio_title, :audio_description, :audio_hosts, :audio_program, :origin_url, :source, :playtime, :status, :finished
-
-    def after
-      'todo'
-    end
+    attributes :after_id, :audio_url, :audio_identifier, :audio_title, :audio_description, :audio_hosts, :audio_program, :origin_url, :source, :playtime, :status, :finished
   end
 end
